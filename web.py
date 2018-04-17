6511ac6c6b4a71949b7770f51f149bde75bba283
Songmingda <songmingda1234@hotmail.com>
2018-04-17 20:25:00 +0800

    modified:   web.py 	Tcpserv.py

--- web.py
+++ web.py
@@ -1,8 +1,8 @@
 # coding: UTF-8
 from flask import Flask, request, session, redirect, send_from_directory
-from time import localtime, strftime
-import csv, codecs, sys, socket, threading
+import csv, codecs, sys
 from global_vars import *
+from Tcpserv import Tcpserv
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -55,28 +55,7 @@ def valveHandle():
 		return redirect('/')
 
 	return ''
-'''
-# 提交新数据
-# data: /post?id=1&temp=20&humid=50&lux=1000&spd=3&valve=5
-@app.route('/post', methods = ['GET'])
-def post():
-	id = request.args['id']
-	temp = request.args['temp']
-	humid = request.args['humid']
-	lux = request.args['lux']
-	spd = request.args['spd']
-	valve = request.args['valve']
 
-	date = strftime("%Y-%m-%d", localtime())
-	time = strftime("%H:%M:%S", localtime())
-	if not clients.has_key(id):
-		clients[id] = Client(temp, humid, lux, spd, valve, time, date)
-	else:
-		clients[id].update(temp, humid, lux, spd, valve, time, date)
-
-	db.addData(id, temp, humid, lux, spd, valve, time, date)
-	return ''
-'''
 # 下载记录
 @app.route('/getLog', methods = ['POST', 'GET'])
 def getLog():
@@ -122,36 +101,6 @@ def logout():
 
 
 #----------各种处理函数----------
-# 提交数据
-# TODO: 建立TCP服务器和多线程来处理设备提交的新数据，提交完后返回电磁阀状态
-# id=1,temp=20,humid=50,lux=1000,spd=3,valve=5
-def post(sock, addr):
-	print '%s:%s established'%addr
-	while True:
-		rawdata = sock.recv(1024)
-		if rawdata == 'bye':
-			break
-		data = rawdata.split(',')
-		valve = data[5]
-		data = {"id":  data[0],
-				"temp": data[1],
-				"humid": data[2],
-				"lux": data[3],
-				"spd": data[4]}
-		data["date"] = strftime("%Y-%m-%d", localtime())
-		data["time"] = strftime("%H:%M:%S", localtime())
-
-		#import pdb; pdb.set_trace()
-		id = data["id"]
-		if not clients.has_key(id):
-			clients[id] = Client(valve, **data)
-		else:
-			clients[id].update(**data)
-		sock.send(clients[id].valve)
-		db.addData(valve = clients[id].valve, **data)
-	print '%s:%s closed'%addr
-	sock.close()
-
 # 电磁阀状态
 def getValveStat(input):
 	input = int(input)
@@ -167,28 +116,16 @@ def getValveStat(input):
 # 保存文件
 def saveFile(data, fr, to):
 	fname = '%s_%s.csv'%(fr, to)
-	with codecs.open(fname, 'wb', "gbk") as csvfile:
+	with codecs.open(fname, 'wb', 'gbk') as csvfile:
 		writer = csv.writer(csvfile, dialect = 'excel')
 		writer.writerow(['设备编号', '温度', '湿度', '照度', '风速', '电磁阀', '时间', '日期'])
 		for i in data:
 			writer.writerow(i)
 	return fname
-
-# socket独立线程
-def recvThd():
-	recv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	recv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	recv.bind(('localhost', 1234))
-	recv.listen(maxClients)
-	while True:
-		sock, addr = recv.accept()
-		t = threading.Thread(target = post, args=(sock, addr))
-		t.start()
 #--------------------------------
 
 # 主函数
 if __name__ == '__main__':
-	t = threading.Thread(target = recvThd)
-	t.setDaemon(True)
-	t.start()
+	serv = Tcpserv(clients, db)
+	serv.start()
 	app.run(port = 8000, debug = True, use_reloader = False)
